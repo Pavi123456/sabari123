db7ed98a1731a352f49962d172b14c714d6547a6
sabariguvi <35296176+sabariguvi@users.noreply.github.com>
2018-01-19 16:02:57 +0530

    Create array.c

---
+++ array.c
@@ -0,0 +1,17 @@
+# include<stdio.h>
+int main()
+{
+int b[5],n,i;
+float sum=0;
+printf("enter the number");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+scanf("%d",&b[i]);
+}
+for(i=1;i<=n;i++)
+{
+sum=sum=b[i];
+}
+return 0;
+}
