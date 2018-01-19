de9612e16381a84dc5272e802d5bd489da52ae4e
sabariguvi <35296176+sabariguvi@users.noreply.github.com>
2018-01-19 15:56:04 +0530

    Create vowel.c

---
+++ vowel.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+char a;
+printf("enter the a");
+scanf("%c",&a);
+if(a=="a",a=="e",a=="i",a=="o",a=="u")
+{
+printf("vowel");
+}
+else
+{
+printf("consonant");
+}
+
+}
