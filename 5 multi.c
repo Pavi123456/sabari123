77f0fdfd6011a5bb1c60ebdf6a3533afb5bfc666
sabariguvi <35296176+sabariguvi@users.noreply.github.com>
2018-01-21 15:01:22 +0530

    Create 5 multi.c

---
+++ 5 multi.c
@@ -0,0 +1,13 @@
+# include <stdio.h>
+void main()
+{ 
+int i, d,a ;   
+printf(" \nEnter the num ") ; 
+scanf("%d",&d) ; 
+printf(" \n The num are ") ; 
+for(i=1;i<=7;i++) 
+{
+a=d*i;
+printf("%d",res) ; 
+}
+}
