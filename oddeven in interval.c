b595bc7b580a80f654500ecf5691c6790165ce10
sabariguvi <35296176+sabariguvi@users.noreply.github.com>
2018-01-21 14:09:47 +0530

    Create oddeven in interval.c

---
+++ oddeven in interval.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+int n1,n2,i;
+printf("Enter the intervals \n");
+scanf("%d%d",&n1,&n2);
+for(i=n1;i<=n2;i++)
+{
+if(i%2==0)
+{
+printf("%d is even",i);
+}
+else
+{
+  printf("%d is odd",i);
+}
+}
+}
