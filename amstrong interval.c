0e49e720e62d7aa15d4a4ada94ee64b46929c125
sabariguvi <35296176+sabariguvi@users.noreply.github.com>
2018-01-21 14:37:36 +0530

    Create amstrong interval.c

---
+++ amstrong interval.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+void main()
+{
+int n,k,temp,count=0;
+scanf("%d",&n);
+k=n;
+while(k!=0)
+{
+temp=k%10;
+count=count+(temp*temp*temp);
+k=k/10;
+}
+if(count==n)
+{
+printf("num is armstrang");
+}
+else
+{
+printf("not armstrang");
+}
+}
