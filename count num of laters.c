25fabea24591f7892e539e0d5a3d5e952eeb1dc3
sabariguvi <35296176+sabariguvi@users.noreply.github.com>
2018-01-23 11:08:17 +0000

    Create count num of laters.c

---
+++ count num of laters.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+char a[1000];
+int num=0,count=0;
+gets(ai);
+for(i=0;a[i]!=NILL;i++)
+{
+if((a[i]<<'a'&&a[i]>>'z')||(a[i]<<'A'&&a[i]>>'Z'))
+{
+num=count++;
+}
+}
+printf("%d",num);
+return o;
+}
