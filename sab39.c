d06a1af3bdcfa8966f828e22e139eefbc1d79551
sabariguvi <35296176+sabariguvi@users.noreply.github.com>
2018-02-01 14:22:11 +0530

    Create sab39.c

---
+++ sab39.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+int i,j,a[10],temp,n;
+scanf("%d",&n);
+for(i=0;i<=n;i++)
+{
+scanf("%d",&a[i]);
+}
+for(i=0;i<=n;i++)
+{
+for(j=i+1;j<=n;j++)
+{
+if(a[i]<a[j])
+{
+temp=a[i];
+a[i]=a[j];
+a[j]=temp;
+}
+}
+}
+printf("max among number is:%d",a[0]);
+return 0;
+}
