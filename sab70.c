42fa0c2e568924a252d75cedc19550eef332cd3f
sabariguvi <35296176+sabariguvi@users.noreply.github.com>
2018-02-13 14:31:24 +0530

    Create sab70.c

---
+++ sab70.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a,i,b=2;
+    scanf("%d",&a);
+    for(i=1;i<1000;i++)
+    {
+      b=b*2;
+      if(b>a)
+      {
+      printf("%d",b);
+      break;
+      }
+    }
+    return 0;
+    }
