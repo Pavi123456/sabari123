0a658dd63aa003d922d2039524747aa3277c9d80
sabariguvi <35296176+sabariguvi@users.noreply.github.com>
2018-02-20 10:08:17 +0000

    Create sabari71.c

---
+++ sabari71.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+     char b[10],a[20];
+     scanf("%s",&a);
+    printf("%s\n",a);
+   strcpy(b,a);  printf("%s\n",b);
+     strrev(b);
+  printf("%s\n",c);
+  if( strcmp(a,b) == 0 )
+    printf("palindrom");
+    else 
+    printf("not a palindrom");
+    return 0;
+    
+}
