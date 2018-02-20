58398824375d9f943f2eed117cda6f20843b20fa
sabariguvi <35296176+sabariguvi@users.noreply.github.com>
2018-02-20 10:50:49 +0000

    Create sab73.c

---
+++ sab73.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+	int a,b,c,i;
+	scanf("%d%d",&a,&b);
+	scanf("\n%d",&c);
+
+		if(c>=a&&c<=b)
+		{
+		printf("yes");
+		
+		}
+		else
+		{
+		printf("no");
+	
+		}
+
+	
+	return 0;
+}
