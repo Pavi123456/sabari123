5cf5ea18fffb129f16eb792eee402c47032ea1d1
sabariguvi <35296176+sabariguvi@users.noreply.github.com>
2018-02-27 10:39:17 +0000

    Create sab83.c

---
+++ sab83.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() 
+{
+	int a,b,c,d;
+	scanf("%d%d",&a,&b);
+	scanf("%d%d",&c,&d);
+	printf("\n%d",a/b);
+  printf("\n%d",a%b);
+	printf("\n%d",c/d);
+	printf("\n%d",c%d);
+	return 0;
+}
